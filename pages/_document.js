e31b729f2fc429a3a0dfcc40ef72acf16e1f7535
johnpiapian <johnpiapian@gmail.com>
2023-01-13 20:34:43 -0600

    Update about page

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,32 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body class="bg-gray-200 font-sans leading-normal tracking-normal">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head />
+            <nav class="bg-gray-800 p-2 mt-0 w-full">
+                <div class="container mx-auto flex flex-wrap items-center">
+                    <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+                        <a class="text-white no-underline hover:text-white hover:no-underline" href="#">
+                            <span class="text-2xl pl-2"><i class="em em-grinning"></i> Ciimnuai</span>
+                        </a>
+                    </div>
+                    <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
+                        <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+                            <li class="mr-3">
+                                <a class="inline-block p-2 text-white no-underline" href="/">Home</a>
+                            </li>
+                            <li class="mr-3">
+                                <a class="inline-block p-2 text-white no-underline" href="/about">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <body class="bg-gray-200 font-sans leading-normal tracking-normal">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -14,22 +14,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
     </Head>
-	<nav class="bg-gray-800 p-2 mt-0 w-full">
-		<div class="container mx-auto flex flex-wrap items-center">
-			<div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
-				<a class="text-white no-underline hover:text-white hover:no-underline" href="#">
-					<span class="text-2xl pl-2"><i class="em em-grinning"></i> Ciimnuai</span>
-				</a>
-			</div>
-			<div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
-				<ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
-					<li class="mr-3">
-						<a class="inline-block py-2 px-4 text-white no-underline" href="#">About</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
     <main>
         <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
             <div class="flex flex-col w-full lg:w-1/2 justify-center items-start pt-12 pb-24 px-6">
