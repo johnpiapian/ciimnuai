0262d9c5118884a318876dc3e1d2fcff5d9d3c38
johnpiapian <johnpiapian@gmail.com>
2023-01-22 18:20:58 -0600

    Update header

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,14 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <nav class="bg-gray-800 p-2 mt-0 w-full">
+            <nav class="bg-[#686868] p-4 mt-0 w-full">
                 <div class="container mx-auto flex flex-wrap items-center">
                     <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
                         <a class="text-white no-underline hover:text-white hover:no-underline" href="#">
                             <span class="text-2xl pl-2"><i class="em em-grinning"></i> Ciimnuai</span>
                         </a>
                     </div>
-                    <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
+                    <div class="flex w-full content-center justify-between md:w-1/2 md:justify-end">
                         <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
                             <li class="mr-3">
                                 <Link class="inline-block p-1 px-2 text-white no-underline" href="/">Home</Link>
